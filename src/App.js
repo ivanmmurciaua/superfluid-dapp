9c1a59f93e1044839fc0cb9d4d43c6342942a64f
Iván M.M <solucionesemecuadrado@gmail.com>
2022-11-04 18:10:54 +0100

    FE working but not stream
    Error: a provider or signer is needed to resolve ENS names (operation="resolveName", code=UNSUPPORTED_OPERATION, version=contracts/5.7.0)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { CreateFlow } from "./createFlow";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <CreateFlow />
+      </div>
+    </div>
+  );
+}
+
+export default App;
